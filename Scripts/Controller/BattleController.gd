754bb7621f177a3b318fdc76c24678605e68f07b
Fabian Urbina <fabian2357@gmail.com>
2026-04-04 16:16:23 -0300

    State Machine

--- Scripts/Controller/BattleController.gd
+++ Scripts/Controller/BattleController.gd
@@ -4,3 +4,8 @@ class_name BattleController
 @export var board: Board
 @export var inputController: InputController
 @export var cameraController: CameraController
+@export var stateMachine: StateMachine
+@export var startState: State
+
+func _ready() -> void:
+	stateMachine.ChangeState(startState)
--- Scripts/Controller/CameraController.gd
+++ Scripts/Controller/CameraController.gd
@@ -12,22 +12,22 @@ var _zoom = 10
 var _minPitch = -90
 var _maxPitch = 0
 
-func _ready():
-	_owner = get_node("../")
-	Zoom(0)
-	AddListeners()
-	
-func _exit_tree():
-	RemoveListeners()
-	
-func AddListeners():
-	_owner.inputController.cameraZoomEvent.connect(Zoom)
-	_owner.inputController.cameraRotateEvent.connect(Orbit)
-	
-func RemoveListeners():
-	_owner.inputController.cameraZoomEvent.disconnect(Zoom)
-	_owner.inputController.cameraRotateEvent.disconnect(Orbit)
-	
+#func _ready():
+	#_owner = get_node("../")
+	#Zoom(0)
+	#AddListeners()
+	#
+#func _exit_tree():
+	#RemoveListeners()
+	#
+#func AddListeners():
+	#_owner.inputController.cameraZoomEvent.connect(Zoom)
+	#_owner.inputController.cameraRotateEvent.connect(Orbit)
+	#
+#func RemoveListeners():
+	#_owner.inputController.cameraZoomEvent.disconnect(Zoom)
+	#_owner.inputController.cameraRotateEvent.disconnect(Orbit)
+	#
 func Zoom(scroll: int):
 	_zoom = clamp(_zoom + scroll, _minZoom, _maxZoom)
 	
